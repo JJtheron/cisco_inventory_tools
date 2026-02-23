9f1fb70f1b5c880e5a3182541d317b7d6c3b2503
jjtheron <theronjanjacques@gmail.com>
2026-02-23 14:56:12 -0800

    Graph Crawler is crawling

--- Create_Graph_crawler.py
+++ Create_Graph_crawler.py
@@ -13,7 +13,7 @@ from pyats.utils.secret_strings import SecretString
 
 
 class Crawl_create:
-    def __init__(self,test_bed_name = "default", os="ios", user = "", password = "", device_name = "first_device", ip_address = "", mgm_vlan="115"):
+    def __init__(self,test_bed_name = "default", os="ios", user = "", password = "", device_name = "first_device", ip_address = ""):
         if not user:
             user = input("Username: ")
         if not password:
@@ -22,27 +22,58 @@ class Crawl_create:
         self.os = "ios"
         self.password = password
         self.graph = nx.MultiGraph()
-        self.mgm_vlan = "Vlan"+mgm_vlan
-        self.__explore_first_switch(device_name,ip_address)
+        self.test_bed_name = test_bed_name
+        self.__explore_first_switch(device_name,[ip_address])
     
     def __explore_first_switch(self, device_name, ip_address):
-        first_device = self._create_Testbed_device(device_name, ip_address)
-        cdp, version, vlanIP, connected = self._get_cdp_info(first_device)
+        cdp = {}
+        version = {}
+        vlanIP = {}
+        connected = False
+        ip_working = ""
+        for ip in ip_address:
+            first_device = self._create_Testbed_device(device_name, ip)
+            cdp, version, vlanIP, connected = self._get_cdp_info(first_device)
+            ip_working = ip
+            if connected: break
 
         if connected:
-            self.__cdp_crawler(self._get_ID(vlanIP,version),cdp,version,vlanIP,None)
+            id = self._create_standard_name(version["version"]["hostname"],ip_working)
+            self.__cdp_crawler(id,cdp,version,vlanIP,None)
+    
+    def _create_standard_name(self,current_switch_name,ip_working):
+        name_standard  = current_switch_name
+        id = name_standard+"\n"+ip_working +"\n"
+        return id
+
+    def __visited(self,neighbor,visited):
+        for visit in visited:
+            try:
+                if visit == neighbor:
+                    return True
+            except:
+                return True
+
+        return False
 
         
-    def __cdp_crawler(self,start,cdp, version, vlanIP, visited):
+    def __cdp_crawler(self,id,cdp, version, vlanIP, visited):
         if visited is None:
             visited = set()
-        visited.add(start)
-        for neighbor in cdp["index"]:
-            if neighbor not in cdp:
-                    first_device = self._create_Testbed_device(neighbor["device_name"], neighbor["ip_address"])
-                    cdp1, version1, vlanIP1, connected1 = self._get_cdp_info(first_device)
+        visited.add(id)
+        self._add_cdp_device_to_graph(id, cdp,version)
+        for index in cdp["index"]:
+            if len(list(cdp["index"][index]["entry_addresses"].keys())) > 0:
+                ip_address = list(cdp["index"][index]["entry_addresses"].keys())[0]
+                next_device = self._create_Testbed_device(cdp["index"][index]["device_id"], ip_address)
+                if not self.__visited(next_device,visited) and not self.__Test_is_router(cdp,index):
+                    ip_address = ""
+                    cdp1, version1, vlanIP1, connected1 = self._get_cdp_info(next_device)
                     if connected1:
-                        self.__cdp_crawler(version1["version"]["chassis_sn"],cdp1,version1,vlanIP1,visited)
+                        id = self._create_standard_name(version1["version"]["hostname"],ip_address)
+                        self.__cdp_crawler(id,cdp1,version1,vlanIP1,visited)
+                    else:
+                        ip_address = ""
 
     
     def _create_Testbed_device(self,new_device_name,ip_address):
@@ -57,13 +88,6 @@ class Crawl_create:
                         )
         return new_device
     
-    def _get_ID(self,vlanIP,version):
-        try:
-            mgmIP = vlanIP[self.mgm_vlan]["ipv4"].keys()[0]
-            return mgmIP
-        except Exception as e:
-            sys.stderr.write(f"Could not find mgmvlan {self.mgm_vlan} {version["version"]["hostname"]} Error is {e} will use host_name as ID")
-            return version["version"]["hostname"]
     
     def _get_cdp_info(self,device):
         command = 'show cdp nei detail'
@@ -79,13 +103,43 @@ class Crawl_create:
         except Exception as e:
             sys.stderr.write(f"Could not connect to device {device} Error is {e}")  
             traceback.print_exc() 
-            return {},{}, False    
+            return {},{},{}, False    
         parse_object = GenieCommandParse(nos=dev.os)
         cdp_parsed =  parse_object.parse_string(show_command = command, show_output_data = cdp)
         version_parsed =  parse_object.parse_string(show_command = command2, show_output_data = version)
         vlanIP_parsed =  parse_object.parse_string(show_command = command3, show_output_data = VlanIP)
         return cdp_parsed, version_parsed,vlanIP_parsed, True
 
+
+    def _add_cdp_device_to_graph(self, id ,cdp_object,version):
+        current_switch_model = version["version"]["chassis"]
+        current_switch_SN = version["version"]["chassis_sn"]
+        current_switch_FW = version["version"]["switch_num"]["1"]["sw_ver"]
+        my_os = version["version"]["os"]
+        self.graph.add_node(id,shape="box",label=f"""{id}
+{current_switch_model}
+{current_switch_SN}
+{my_os} {current_switch_FW}""",color="black")
+        ip_address = ""
+        for index in cdp_object['index']:
+            new_device_name =  cdp_object['index'][index]['device_id'].split(".")[0]
+            edge_label, local_port, remote_port =  self.__create_port_label(cdp_object,index)
+            try: 
+                if len(list(cdp_object['index'][index]["entry_addresses"].keys())) > 0:
+                    ip_address = list(cdp_object['index'][index]["entry_addresses"].keys())[0]
+            except:
+                ip_address = ""
+                print(f"{new_device_name} does not have a IP address!!!------------------------<<<<<<<<<<<<")
+            new_switch_id = self._create_standard_name(new_device_name,ip_address)
+            if cdp_object['index'][index]['capabilities'].lower().find("switch")>=0 and not self.__Test_is_router(cdp_object,index):
+                if not self.__edges_exists(local_port,remote_port,id,new_switch_id):
+                    ## JJ was here
+                    self.graph.add_edge(id,new_switch_id,label = edge_label)
+                    self.graph.add_node(new_switch_id,shape="box",label=f"""{new_switch_id}""",color="red")
+
+#########################################################################################
+#vvvvvvvvvvvvvvvvvvvvvvv Helper functions go here  vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+#########################################################################################
     def __shorten_edge_name(self,port):
         try:
             portType = port[:2]
@@ -103,46 +157,20 @@ class Crawl_create:
                     return True
         return False
 
-    def _add_cdp_device_to_testbed(self, cdp_object, device,version):
-        ip_address = ""
-        for index in cdp_object['index']:
-            new_device_name =  cdp_object['index'][index]['device_id'].split(".")[0] 
-            new_device_name_g = new_device_name
-            if len(new_device_name.split("-")) > 1:
-                new_device_name_g = new_device_name[new_device_name.find("-"):].lstrip("-")
-            device_g = device
-            if len(device.split("-")) > 1:
-                device_g = device[device.find("-"):].lstrip("-")
-            software_version = cdp_object["index"][index]["software_version"]
-            local_port = self.__shorten_edge_name(cdp_object['index'][index]['local_interface'])
-            remote_port = self.__shorten_edge_name(cdp_object['index'][index]['port_id'])
-            edge_label =  f"{local_port}->{remote_port}"
-            try: 
-                ip_address = list(cdp_object['index'][index]["management_addresses"].keys())[0]
-            except:
-                ip_address = ""
-                print(f"{cdp_object['index'][index]['device_id']} does not have a IP address!!!------------------------<<<<<<<<<<<<")
-            my_os = "ios" if re.search("ios",software_version,re.IGNORECASE) else software_version.split(",")[0]
-            if cdp_object['index'][index]['capabilities'].lower().find("switch")>=0: # and cdp_object['index'][index]['capabilities'].lower().find("router")<0:
-                if not self.__edges_exists(local_port,remote_port,device_g,new_device_name_g):
-                    self.graph.add_edge(device_g,new_device_name_g,label = edge_label)
-                    self.graph.add_node(new_device_name_g,shape="box",label=f"""{new_device_name_g}
-{cdp_object['index'][index]['platform']}""")
-                    
-                self.graph.add_node(device_g,shape="box",label=f"""{device_g}
-{testbed.devices[device].connections['cli']['ip']}
-{version['version']['chassis']}
-{version['version']['chassis_sn']}
-{version['version']['version']}""")
-                new_device = Device(new_device_name,
-                                         os = my_os,
-                                         connections = {'cli':
-                                                        {'protocol':'ssh',
-                                                      'ip' : ip_address}},
-                                        credentials = testbed.devices[device].credentials,
-                                        )
+    def __create_port_label(self,cdp_object,index):
+        local_port = self.__shorten_edge_name(cdp_object['index'][index]['local_interface'])
+        remote_port = self.__shorten_edge_name(cdp_object['index'][index]['port_id'])
+        return f"{local_port}->{remote_port}", local_port, remote_port
+    
+    def __Test_is_router(self,cdp_object,index):
+        return "router" in cdp_object['index'][index]['capabilities'].lower() or "phone" in cdp_object['index'][index]['capabilities'].lower()
+#######################################################################################################################
+#vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv Output functions go here vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+#######################################################################################################################
 
-
+    def print_map(self):
+        viz = nx.nx_agraph.to_agraph(self.graph)
+        viz.draw(f"{self.test_bed_name}.png",prog="dot")
 
 if __name__ == "__main__":    
     fire.Fire(Crawl_create)
