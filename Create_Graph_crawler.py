97b6f5f7353a9078ff257bfc5777cbcfd32f03b6
jjtheron <theronjanjacques@gmail.com>
2026-05-21 14:08:53 -0700

    Added location of switches and updated requirements.

--- Create_Graph_crawler.py
+++ Create_Graph_crawler.py
@@ -41,19 +41,19 @@ class Crawl_create:
         monitor_info_parsed = {}
         for ip in ip_address:
             first_device = self._create_Testbed_device(device_name, ip)
-            cdp, version, Trunk, connected, monitor_info_parsed = self._get_cdp_info(first_device)
+            cdp, version, Trunk, connected, monitor_info_parsed, snmp_location = self._get_cdp_info(first_device)
             ip_working = ip
             if connected: break
 
         if connected:
             id = self._create_standard_name(version["version"]["hostname"],ip_working)
-            self.__cdp_crawler(id,cdp,version,Trunk,None, monitor_info_parsed)
+            self.__cdp_crawler(id,cdp,version,Trunk,None, monitor_info_parsed, snmp_location)
 
         
-    def __cdp_crawler(self,id,cdp, version, Trunk, visited,monitor_info_parsed):
+    def __cdp_crawler(self,id,cdp, version, Trunk, visited,monitor_info_parsed, snmp_location):
         if visited is None:
             visited = []
-        self._add_cdp_device_to_graph(id, cdp,version,Trunk,monitor_info_parsed)
+        self._add_cdp_device_to_graph(id, cdp,version,Trunk,monitor_info_parsed, snmp_location)
         visited.append(id)
         for index in cdp["index"]:
             if len(list(cdp["index"][index]["entry_addresses"].keys())) > 0:
@@ -61,10 +61,10 @@ class Crawl_create:
                 next_device = self._create_Testbed_device(cdp["index"][index]["device_id"], ip_address)
                 next_device_id = self._create_standard_name(cdp["index"][index]["device_id"].split(".")[0], ip_address)
                 if not self.__visited(next_device_id,visited) and not self.__Test_is_router(cdp,index):
-                    cdp1, version1, Trunk1, connected1, monitor_info_parsed1 = self._get_cdp_info(next_device)
+                    cdp1, version1, Trunk1, connected1, monitor_info_parsed1, snmp_location1 = self._get_cdp_info(next_device)
                     if connected1:
                         id = self._create_standard_name(version1["version"]["hostname"],ip_address)
-                        self.__cdp_crawler(id,cdp1,version1,Trunk1,visited,monitor_info_parsed1)
+                        self.__cdp_crawler(id,cdp1,version1,Trunk1,visited,monitor_info_parsed1, snmp_location1)
                     else:
                         ip_address = ""
     
@@ -73,6 +73,7 @@ class Crawl_create:
         command2 = "show version"
         command3 = "show interface trunk"
         command4 = "show monitor session remote"
+        command5 = "show snmp location"
         try:
             dev = device
             dev.connect(learn_hostname=True,goto_enable=False,init_exec_commands=[],init_config_commands=[],log_stdout=False)
@@ -81,25 +82,28 @@ class Crawl_create:
             trunk_info = dev.default.execute(command3)
             try:monitor_info = dev.default.execute(command4)
             except:monitor_info = ""
+            try: snmp_location = dev.default.execute(command5)
+            except: snmp_location = "No SNMP Location"
             dev.disconnect()
         except Exception as e:
             sys.stderr.write(f"Could not connect to device {device} Error is {e}")  
             traceback.print_exc() 
-            return {},{},{}, False,{}    
+            return {},{},{}, False,{},"No SNMP Location"   
         parse_object = GenieCommandParse(nos=dev.os)
         cdp_parsed =  parse_object.parse_string(show_command = command, show_output_data = cdp)
         version_parsed =  parse_object.parse_string(show_command = command2, show_output_data = version)
         trunk_info_parsed =  parse_object.parse_string(show_command = command3, show_output_data = trunk_info)
         monitor_info_parsed =  re.findall(r'Dest RSPAN VLAN\s*:\s*(\d+)', monitor_info)
-        return cdp_parsed, version_parsed,trunk_info_parsed, True, monitor_info_parsed
+        return cdp_parsed, version_parsed,trunk_info_parsed, True, monitor_info_parsed, snmp_location
 
 
-    def _add_cdp_device_to_graph(self, id ,cdp_object,version,Trunk,monitor_info_parsed):
+    def _add_cdp_device_to_graph(self, id ,cdp_object,version,Trunk,monitor_info_parsed, snmp_location):
         current_switch_model = version["version"]["chassis"]
         current_switch_SN = version["version"]["chassis_sn"]
         current_switch_FW = version["version"]["version"]
         my_os = version["version"]["os"]
         self.graph.add_node(id,shape="box",label=f"""{id}
+{snmp_location}
 {current_switch_model}
 {current_switch_SN}
 {my_os} {current_switch_FW}
